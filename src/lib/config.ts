e5f546e468c3a8b18c5f396097ccdb1db47f7c53
dev-hector <hector.lrugel@icloud.com>
2026-06-19 08:40:33 -0500

    listo

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -5,7 +5,7 @@ export const siteConfig = {
   whatsappHref: 'https://wa.me/16263413078',
   city: 'Los angeles',
   state: 'California',
-  email: 'hrugel@uees.edu.ec',
+  email: 'juliojunkremoval@gmail.com',
   facebook: '#',
   instagram: 'https://www.instagram.com/juliojunkr/',
   tiktok: '#',
